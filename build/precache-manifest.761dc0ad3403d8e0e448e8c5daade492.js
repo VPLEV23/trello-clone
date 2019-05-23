7b815c0d43c4e11db034d9e6b9a12492ec45e5d2
Vlad <bladetheblade12@gmail.com>
2019-05-23 17:49:58 +0200

    trello clone

---
+++ build/precache-manifest.761dc0ad3403d8e0e448e8c5daade492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab30a628abb805a2298235f8521b4890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dda3f41a75f49346a093",
+    "url": "/static/css/main.39b1b0a5.chunk.css"
+  },
+  {
+    "revision": "bc02efc96a53be6cecde",
+    "url": "/static/js/2.91df2c66.chunk.js"
+  },
+  {
+    "revision": "dda3f41a75f49346a093",
+    "url": "/static/js/main.7d2f6bfe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
